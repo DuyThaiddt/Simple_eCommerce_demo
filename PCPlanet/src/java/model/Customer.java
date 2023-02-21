a8523da2d99d42bfe840d5bf1e1182892f261ec6
Duy Thai <daoduythai.business@gmail.com>
2023-02-21 09:16:28 +0700

    minor changes

---
+++ PCPlanet/src/java/model/Customer.java
@@ -0,0 +1,33 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package model;
+
+/**
+ *
+ * @author Duy Thai
+ */
+public class Customer {
+
+    private Integer id;
+    private String name; 
+    private String username, password;
+    private String email;
+
+    public Customer() {
+    }
+
+    public Customer(Integer id, String name, String username, String password, String email) {
+        this.id = id;
+        this.name = name;
+        this.username = username;
+        this.password = password;
+        this.email = email;
+    }
+    
+        
+
+    
+
+}
